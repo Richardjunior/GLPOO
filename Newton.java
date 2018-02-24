8ceca79d03bc30de3cd0bac1a19705c5f4de89fd
MandarSy <32954876+ArmandSy@users.noreply.github.com>
2018-02-24 17:36:52 +0100

    Newton efficient
    computes faster

--- Newton.java
+++ Newton.java
@@ -1,5 +1,3 @@
-
-
 import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.Graphics;
@@ -10,29 +8,31 @@ import org.apache.commons.math3.complex.Complex;
 import javax.swing.JFrame;
 import javax.swing.JComponent;
 
-public class Main extends JComponent
+public class Newton extends JComponent
 {
 	
 	public static void main(String[] args)
 	{
-		new Main();
+		new Newton();
 	}
 	
-	public static final int LARGEUR = 620; // Définition de la largeur de la fenetre
-	public static final int HAUTEUR = 480; // '' hauteur de la fenetre
-	public static final int ITERATIONS = 5000; // Définition du nombre d'itérations
+	public static final int LARGEUR = 1280; // Définition de la largeur de la fenetre
+	public static final int HAUTEUR = 720; // '' hauteur de la fenetre
+	public static final int ITERATIONS = 500; // Définition du nombre d'itérations
 	public static final float ECHELLE = 300; // Definition de l'echelle de grandeur
 	public static final Complex r1 = new Complex(1,0); // Racine réelle du polynome de Newton classique
-	public static final Complex r2 = new Complex(-1/2,-Math.sqrt(3)/2); // Racine complexe du polynome de Newton classique
-	public static final Complex r3 = new Complex(-1/2,Math.sqrt(3)/2); // Racine complexe conjuguée
+	public static final Complex r2 = new Complex(-0.5,-Math.sqrt(3)/2); // Racine complexe du polynome de Newton classique
+	public static final Complex r3 = new Complex(-0.5,Math.sqrt(3)/2); // Racine complexe conjuguée
 	
-	public static final float epsilon = (float)0.5; //plus epsilon est petit, plus le calcul est précis, et le temps de calcul évolue de manière exponentielle. Tester avec de petites valeurs au début
+	public static final double epsilon = (double)0.1; //plus epsilon est petit, plus le calcul est précis, et le temps de calcul évolue de manière exponentielle. Tester avec de petites valeurs au début
+	public static final double alpha = (double)1;
+	
 	
 	static Complex [] roots = {r1,r2,r3};
 	
 	private BufferedImage buffer; // Image tampon
 	
-	public Main()
+	public Newton()
 	{
 		buffer = new BufferedImage(LARGEUR, HAUTEUR, BufferedImage.TYPE_INT_RGB); // On instancie le buffer : dimensions et coloration
 		
@@ -64,7 +64,7 @@ public class Main extends JComponent
 			}
 	}
 	
-	public int calculCouleur(float zx, float zy)
+	public int calculCouleur(double zx, double zy)
 	{
 		int n = 0;
 		
@@ -72,22 +72,25 @@ public class Main extends JComponent
 		
 		for(; n < ITERATIONS; n++)
 		{
-			z = z.subtract(((poly(z)).divide(deriv(z))));
+			z = (((z.pow(3)).multiply(2)).add(1)).divide((z.pow(2)).multiply(3)); // z = 2z^3 + 1 / 3z^2
 			for(Complex item : roots)
 			{
-				if(Math.abs(z.getReal()-item.getReal()) < epsilon && Math.abs(z.getImaginary()- item.getImaginary()) < epsilon)
+				if(Math.abs(z.getReal() - item.getReal()) < epsilon && Math.abs(z.getImaginary() - item.getImaginary()) < epsilon)
 						{
 							if(item.getImaginary()==0)
 							{
-								return Color.HSBtoRGB((float)Math.sqrt((double)300*n/ITERATIONS) , 0.5f, 1); // ce point converge vers la racine réelle
+								return Color.HSBtoRGB((float)Math.sqrt((double)40*n/ITERATIONS) , 0.5f, 1); // ce point converge vers la racine réelle
+								//return 0xFF0000FF;
 							}
 							else if(item.getImaginary()<0)
 							{
-								return Color.HSBtoRGB((float)Math.sqrt((double)300*n/ITERATIONS) , 0.5f, 1); // ce point converge vers la 1ere racine complexe
+								return Color.HSBtoRGB((float)Math.sqrt((double)40*n/ITERATIONS) , 0.5f, 1); // ce point converge vers la 1ere racine complexe
+								//return 0xFFFF0000;
 							}
 							else if(item.getImaginary()>0)
 							{
-								return Color.HSBtoRGB((float)Math.sqrt((double)300*n/ITERATIONS) , 0.5f, 1); // ce point converge vers la racine complexe conjuguée
+								return Color.HSBtoRGB((float)Math.sqrt((double)40*n/ITERATIONS) , 0.5f, 1); // ce point converge vers la racine complexe conjuguée
+								//return 0xFF00FF00;
 							}
 						}
 			}
@@ -97,7 +100,7 @@ public class Main extends JComponent
 	
 	public Complex poly(Complex z)
 	{
-		return (z.pow(3)).add(-1);
+		return (z.pow(3)).subtract(1);
 	}
 	
 	public Complex deriv(Complex z)
