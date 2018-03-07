8aef8217ae374ef49efe895683f8465e9c99b94f
Richy <33844596+Richardjunior@users.noreply.github.com>
2018-03-07 14:17:23 +0100

    Update TirageToFractaleConverter.java

--- GLPOOproject/src/main/java/fractales/TirageToFractaleConverter.java
+++ GLPOOproject/src/main/java/fractales/TirageToFractaleConverter.java
@@ -6,7 +6,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 import dao.Tirage;
-import frames.FractaleFrame;
+import frames.FractaleJFrame;
 
 public class TirageToFractaleConverter {
 	private int p; // => b1
