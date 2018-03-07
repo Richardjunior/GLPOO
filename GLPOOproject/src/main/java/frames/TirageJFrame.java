b717939019a9e36bdd620410677bf91d62395ab8
Richy <33844596+Richardjunior@users.noreply.github.com>
2018-03-07 10:57:14 +0100

    Update TirageJFrame.java
    Mise à jour.
    NB , je prends les données uniquement de la dernière version que j'ai reçu de UGO.
    Don't Worry.

--- GLPOOproject/src/main/java/frames/TirageJFrame.java
+++ GLPOOproject/src/main/java/frames/TirageJFrame.java
@@ -1,45 +1,66 @@
 package frames;
 
-
 import java.awt.BorderLayout;
+import java.awt.Color;
 import java.awt.Dimension;
+import java.awt.Font;
 import java.awt.event.ActionEvent;
 import java.util.ArrayList;
 import java.util.List;
 
 import javax.swing.AbstractAction;
-import javax.swing.ButtonGroup;
-import javax.swing.JDialog;
+import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JMenu;
 import javax.swing.JMenuBar;
 import javax.swing.JMenuItem;
-import javax.swing.JOptionPane;
-import javax.swing.JRadioButton;
+import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.JTable;
+import javax.swing.border.Border;
 import javax.swing.table.TableModel;
 import javax.swing.table.TableRowSorter;
 
-import dao.Tirage;
+import model.*;
+import dao.*;
 import fractales.Fractale;
 import fractales.TirageToFractaleConverter;
-import model.ModeleDynamique;
 
 
 
 public class TirageJFrame extends JFrame {
 	private ModeleDynamique modele = new ModeleDynamique();
 	private final JTable tableau = new JTable(modele);
-	private JDialog dialog = new JDialog();
-	
+
 	public TirageJFrame() {
+		
 		super();
 		setTitle("Liste des tirages");
-		setPreferredSize(new Dimension(800, 900));
+		setPreferredSize(new Dimension(1068, 576));
 		setDefaultCloseOperation(EXIT_ON_CLOSE);
-
+		
+		tableau.setBackground(Color.DARK_GRAY);
+		tableau.setForeground(Color.WHITE);
+		tableau.setRowHeight(25);
+		tableau.setFont(new Font("OCR A Extended", Font.PLAIN, 15));
 		getContentPane().add(new JScrollPane(tableau), BorderLayout.CENTER);
+		
+		JPanel buttonPane = new JPanel();
+		buttonPane.setBackground(Color.BLACK);
+		
+		JButton button_julia = new JButton(new GenerateWtihCSV_Julia());
+		button_julia.setBackground(Color.BLACK);
+		button_julia.setForeground(Color.WHITE);
+		
+		buttonPane.add(button_julia);
+		
+		JButton button_mandelbrot = new JButton(new GenerateWtihCSV_Mandelbrot());
+		button_mandelbrot.setBackground(Color.BLACK);
+		button_mandelbrot.setForeground(Color.WHITE);
+		
+		buttonPane.add(button_mandelbrot);
+		
+		getContentPane().add(buttonPane, BorderLayout.SOUTH);
 
 		tableau.setAutoCreateRowSorter(true);
 		final TableRowSorter<TableModel> sorter = new TableRowSorter<TableModel>(tableau.getModel());
@@ -57,7 +78,7 @@ public class TirageJFrame extends JFrame {
 		// MENU 1
 		JMenu menu1 = new JMenu("Fichier");
 		
-		JMenuItem test2 = new JMenuItem(new Dessiner());
+		JMenuItem test2 = new JMenuItem(new GenerateWtihCSV_Julia());
 		menu1.add(test2);
 		
 		JMenuItem backMenu = new JMenuItem(new BackAction("Go back to the previous menu"));
@@ -76,59 +97,78 @@ public class TirageJFrame extends JFrame {
 		public void actionPerformed(ActionEvent arg0) {
 			dispose();
 			WelcomeJFrame f = new WelcomeJFrame();
-			f.setVisible(true);
+			//f.setVisible(true);
 		}
     	
     }
 	
 
-	private class Dessiner extends AbstractAction {
+	private class GenerateWtihCSV_Julia extends AbstractAction {
 		
 		private TirageToFractaleConverter converter;
-		private FractaleFrame fractaleFrame;
+		private FractaleJFrame fractaleFrame;
 
-		private Dessiner() {
-			super("Dessiner");
+		private GenerateWtihCSV_Julia() {
+			super("Generate draw with Julia");
 		}
 		public void actionPerformed(ActionEvent e) {
 			if (tableau.getSelectedRow() > -1) {
+				//System.out.println("Tirage séléctionné");
 				//LOGGER.debug("Tirage séléctionné : ");
 			
 				List<Tirage> tirages = new ArrayList<Tirage>();
 				
 				
 				final int[] selection = tableau.getSelectedRows();
-				
-				if (selection.length > 4) {
-					JOptionPane.showMessageDialog(null,"Trop de lignes sélectionnées, le résultat risque de ne pas être agréable pour les yeux!");
-				}else {
 	
-					for (int i = selection.length - 1; i >= 0; i--) {
-						Tirage tirage = modele.rowToTirage(selection[i]);
-						tirages.add(tirage);
-					}
-
-					Object[] possibilities = {"Julia", "Mandelbrot"};
-					String s = (String)JOptionPane.showInputDialog(
-					                    null,
-					                    "Choose a type of fractale",
-					                    "May the force be with you",
-					                    JOptionPane.PLAIN_MESSAGE,
-					                    null,
-					                    possibilities,
-					                    "Julia");
-
-					//If a string was returned, say so.
-					if ((s != null) && (s.length() > 0)) {
-						dispose();
-						converter = new TirageToFractaleConverter(tirages,s);
-						List<Fractale> fractales = converter.convertTirageToFractale();
-						
-						fractaleFrame = new FractaleFrame(fractales,800,800);
-					}
+				for (int i = selection.length - 1; i >= 0; i--) {
+					Tirage tirage = modele.rowToTirage(selection[i]);
+					tirages.add(tirage);
 				}
+				
+				dispose();
+				converter = new TirageToFractaleConverter(tirages,"Julia");
+				List<Fractale> fractales = converter.convertTirageToFractale();
+				
+				fractaleFrame = new FractaleJFrame(fractales,800,800);
 			}else {
-				JOptionPane.showMessageDialog(null,"Sélectionnez au moins une ligne.");
+				System.out.println("Aucun tirage séléctionné !");
+				//LOGGER.debug("Aucun tirage séléctionné !");
+			}
+		}
+
+	}
+	
+	private class GenerateWtihCSV_Mandelbrot extends AbstractAction {
+		
+		private TirageToFractaleConverter converter;
+		private FractaleJFrame fractaleFrame;
+
+		private GenerateWtihCSV_Mandelbrot() {
+			super("Generate draw with Mandelbrot");
+		}
+		public void actionPerformed(ActionEvent e) {
+			if (tableau.getSelectedRow() > -1) {
+				//System.out.println("Tirage séléctionné");
+				//LOGGER.debug("Tirage séléctionné : ");
+			
+				List<Tirage> tirages = new ArrayList<Tirage>();
+				
+				
+				final int[] selection = tableau.getSelectedRows();
+	
+				for (int i = selection.length - 1; i >= 0; i--) {
+					Tirage tirage = modele.rowToTirage(selection[i]);
+					tirages.add(tirage);
+				}
+				
+				dispose();
+				converter = new TirageToFractaleConverter(tirages,"Mandelbrot");
+				List<Fractale> fractales = converter.convertTirageToFractale();
+				
+				fractaleFrame = new FractaleJFrame(fractales,800,800);
+			}else {
+				System.out.println("Aucun tirage séléctionné !");
 				//LOGGER.debug("Aucun tirage séléctionné !");
 			}
 		}
